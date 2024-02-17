ba52f191e3d1f1d9488eb193ddb9d9a6a876e185
Haussmann000 <duf000simon@gmail.com>
2024-02-17 20:02:06 +0900

    serviceディレクトリ変更

--- services/vpc.go
+++ services/vpc.go
@@ -6,21 +6,24 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 // ...
 
-func DescribeMyVpcs() {
+func DescribeMyVpcs() (vpcs []types.Vpc, err error) {
 
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		log.Fatalf("failed to load configuration, %v", err)
 	}
 	client := ec2.NewFromConfig(cfg)
-	result := client.DescribeVpcs(context.TODO(), &ec2.DescribeVpcsInput{})
-	return result
+	result, err := client.DescribeVpcs(context.TODO(), &ec2.DescribeVpcsInput{})
+	vpcs = result.Vpcs
+
+	return vpcs, err
 }
 
-type Vpc struct {
+type VpcResult struct {
 	VpcId string
 }
